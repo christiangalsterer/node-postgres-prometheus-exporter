20b7463de534ceb57a80533ce0d45263f87ddff8
Christian Galsterer <christian.galsterer@gmx.de>
2024-10-17 11:39:04 +0200

    refactor: use default value for default labels

--- src/utils.ts
+++ src/utils.ts
@@ -6,10 +6,8 @@ import type { Pool } from 'pg'
  * @param defaultLabels default labels to merge with
  * @returns array of merged label names
  */
-export function mergeLabelNamesWithStandardLabels(labelNames: string[], defaultLabels?: Record<string, string | number>): string[] {
-  let merged: string[]
-  defaultLabels !== undefined ? (merged = labelNames.concat(Object.keys(defaultLabels))) : (merged = labelNames)
-  return merged
+export function mergeLabelNamesWithStandardLabels(labelNames: string[], defaultLabels = {}): string[] {
+  return labelNames.concat(Object.keys(defaultLabels))
 }
 
 /**
@@ -20,16 +18,14 @@ export function mergeLabelNamesWithStandardLabels(labelNames: string[], defaultL
  */
 export function mergeLabelsWithStandardLabels(
   labels: Record<string, string | number | undefined>,
-  defaultLabels?: Record<string, string | number>
+  defaultLabels = {}
 ): Record<string, string | number> {
-  let merged: Record<string, string | number>
   const filtered = Object.fromEntries(
     Object.entries(labels)
       .filter(([key, value]) => value !== undefined)
       .map(([key, value]) => [key, value!])
   ) as Record<string, string | number>
-  defaultLabels !== undefined ? (merged = { ...filtered, ...defaultLabels }) : (merged = filtered)
-  return merged
+  return { ...filtered, ...defaultLabels }
 }
 
 /**
